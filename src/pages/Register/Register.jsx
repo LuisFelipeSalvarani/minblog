ba657d8fb98184f145461d806e6dbe24ad664629
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-06-18 15:51:33 -0300

    Registrando um usuário

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -2,6 +2,8 @@ import styles from "./Register.module.css"
 
 import { useState, useEffect } from "react"
 
+import { useAutehntication } from "../../hooks/useAuthentication"
+
 const Register = () => {
     const [displayName, setDisplayName] = useState("")
     const [email, setEmail] = useState("")
@@ -9,24 +11,34 @@ const Register = () => {
     const [confirmPassword, setConfirmPassword] = useState("")
     const [error, setError] = useState("")
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+    const {createUser, error: authError, loading } = useAutehntication();
 
-        setError("")
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        setError("");
 
         const user = {
             displayName,
             email,
             password
-        }
+        };
 
         if(password !== confirmPassword) {
-            setError("As senhas precisam ser iguais!")
-            return
+            setError("As senhas precisam ser iguais!");
+            return;
         }
 
-        console.log(user)
+        const res = await createUser(user);
+
+        console.log(res);
     }
+
+    useEffect(() => {
+        if(authError) {
+            setError(authError);
+        }
+    }, [authError]);
 
   return (
     <div className={styles.register}>
@@ -77,7 +89,8 @@ const Register = () => {
                  onChange={(e) => setConfirmPassword(e.target.value)}
                  />
             </label>
-            <button className="btn">Cadastrar</button>
+            {!loading && <button className="btn">Cadastrar</button>}
+            {loading && <button className="btn" disabled>Aguarde...</button>}
             {error && <p className="error">{error}</p>}
         </form>
     </div>
