ab4519e5bddc02109be17532a43439258dc96ee8
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-06-18 15:55:37 -0300

    Context de autenticação

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,36 @@
-import './App.css'
+import './App.css';
 
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// context
+import { AuthProvider } from './context/AuthContext';
 
 // pages
-import Home from './pages/Home/Home'
-import About from './pages/About/About'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Login from './pages/Login/Login'
-import Register from './pages/Register/Register'
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
-      <Navbar />
-        <div className="container">
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }
